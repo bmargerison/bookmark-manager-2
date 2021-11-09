e2651e82ab1c4e6cb683d240846050262e6ff5f6
bmargerison <bmargerison@outlook.com>
2021-11-09 15:37:15 +0000

    Setting up test database

--- Gemfile
+++ Gemfile
@@ -4,12 +4,17 @@ source "https://rubygems.org"
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-group :development, :test do
-  gem 'rubocop', '1.20'
+group :test do
   gem 'capybara'
   gem 'sinatra'
+  gem 'sinatra-reloader'
   gem 'sinatra-contrib'
   gem 'rspec'
   gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
+
+group :development, :test do
+  gem 'rubocop', '1.20'
   gem 'pg'
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,7 +2,9 @@ require './lib/bookmark.rb'
 require 'pg'
 
 describe Bookmark do 
-    it 'returns the list of all bookmarks' do
-        expect(Bookmark.all).to include 'https://makers.tech/'
-    end 
+	it 'returns the list of all bookmarks' do
+			connection = PG.connect(dbname: 'bookmark_manager_test')
+			connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+			expect(Bookmark.all).to include("http://www.makersacademy.com")
+	end 
 end
